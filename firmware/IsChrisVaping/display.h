2a248fcccb22e8bab1908b4f75cd0b16aa969408
Jim Wild <james.wild6@nhs.net>
2026-07-09 21:24:56 +0100

    Add offsets to the corner elements (#24)

--- firmware/IsChrisVaping/display.h
+++ firmware/IsChrisVaping/display.h
@@ -22,6 +22,12 @@ static constexpr uint8_t lineSpacing = 2;
 extern Adafruit_ST7789 tft;
 extern const char* currentDisplayText;
 
+#define BT_X_OFFSET 10
+#define BT_Y_OFFSET 10
+
+static constexpr int16_t vapeNameXOffset = 2;
+static constexpr int16_t vapeNameYOffset = 2;
+
 void displayInit();
 void showText(const char* text);
 void drawBtIcon();
